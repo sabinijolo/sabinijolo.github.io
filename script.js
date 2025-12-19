318ae30272d41e30d4822a6332966ef87c6c4133
sabinijolo <johnlloyd.flores@lorma.edu>
2025-12-19 22:17:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, this will be my last straw to show you how serious I truly am.\n\nWe both tried. I know this may seem strange, especially since you’ve always been clear with me,.”\n\nbut for once I want you to know that I’m willing to take a risk when it comes to you.\n\nI’m writing this letter to invite you on an actual date with me before the year ends.\n\nIt’s completely okay if you say no, but your presence would mean a lot to me`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, this will be my last straw to show you how serious I truly am.\n\n I know this may seem strange, especially since you’ve always been clear with me,.”\n\nbut for once I want you to know that I’m willing to take a risk when it comes to you.\n\nI’m writing this letter to invite you on an actual date with me before the year ends.\n\nIt’s completely okay if you say no, but your presence would mean a lot to me`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,4 +21,5 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
